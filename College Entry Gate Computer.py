1fb31ce57e4ad5a132c5673bec2cf742f4093d1e
Kartikey Singh <116992721+kartikeysingh7@users.noreply.github.com>
2023-01-11 19:48:45 +0530

    Add files via upload

---
+++ College Entry Gate Computer.py
@@ -0,0 +1,10 @@
+#College entry gate computer
+EntryTime=(1032)
+if (EntryTime<=(1000)):
+    print("You can Enter in College")
+elif ((1100)>=EntryTime>(1000)):
+    print("You are Late, for entry you Need to take Permission")
+elif (1200>=EntryTime>(1100)):
+    print("You can enter college but after giving fine")
+else:
+    print("Get out of here you are suspended")          
